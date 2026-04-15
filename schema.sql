33728693a57cdb8a37635f328fec1a46191a87b4
Nisha-2202 <nishagovintharaj8438@gmail.com>
2026-04-15 03:33:59 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     id         INT AUTO_INCREMENT PRIMARY KEY,
     name       VARCHAR(120) NOT NULL,
     email      VARCHAR(120) NOT NULL UNIQUE,
-    phone      VARCHAR(20),
+    phone      VARCHAR(20) NOT NULL,
     address    TEXT,
     role       ENUM('donor','ngo','admin') NOT NULL DEFAULT 'donor',
     password   VARCHAR(255) NOT NULL,
@@ -31,10 +31,16 @@ CREATE TABLE IF NOT EXISTS requests (
     ngo_id      INT NOT NULL,
     medicine_id INT NOT NULL,
     note        TEXT,
-    status      ENUM('pending','approved','in_transit','delivered') DEFAULT 'pending',
+    status      ENUM('pending','approved','in_transit','delivered',rejected) DEFAULT 'pending',
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (ngo_id) REFERENCES users(id),
     FOREIGN KEY (medicine_id) REFERENCES medicines(id)
+);
+CREATE TABLE IF NOT EXISTS otp_verification (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(120),
+  otp VARCHAR(10),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Default admin account (login: admin@medishare.com / admin123)
@@ -45,5 +51,6 @@ VALUES (
     '9999999999',
     'MediShare HQ',
     'admin',
+    'admin123'
     'scrypt:32768:8:1$D7TUSmaXBdx1NhvM$ccb1c2e828c029a3b25a55fcc8546bdc3b56593143c320a8036eab7c8869d0f675f86bc8e78543446b328e1bfff9c092943f5ce2a3c0c40934f64433602592a1'
 );
